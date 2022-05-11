48d3b4df244395674aef4f184f50b8cd2830caa5
Younes Baali <unessbaali@gmail.com>
2022-05-11 14:13:25 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import store from './src/store/store';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <Provider store={store}>
+          <NavigationContainer>
+         {/* Rest of your app code */}
+         </NavigationContainer>
+        </Provider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, Text, Stylesheet } from 'react-native';
+
+
+export default function card({title}) {
+  return (
+    <View>
+        <Text>{title}</Text>
+    </View>
+  )
+}
+
+const style = Stylesheet.create({
+    container: {
+        height: "10%",
+        width: " 100%",
+        margin: 10,
+    },
+    title: {
+        textAlign
+    }
+})
